0de04f478e0399a13354e220a5b9e032445d43dc
chbaravind04 <118453542+chbaravind04@users.noreply.github.com>
2023-05-21 15:11:02 +0530

    Create optimal_replacement.py

---
+++ optimal_replacement.py
@@ -0,0 +1,29 @@
+class OptimalCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+
+    def get(self, key):
+        if key in self.cache:
+            return self.cache[key]
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            del self.cache[key]  # Remove existing key-value pair
+        elif len(self.cache) >= self.capacity:
+            self.evict()  # Remove a key-value pair based on optimal replacement strategy
+        self.cache[key] = value  # Add new key-value pair
+
+    def evict(self):
+        # Find a key to evict based on optimal replacement strategy
+        future_indices = {key: len(self.cache) for key in self.cache}  # Initialize with maximum future index
+        for i, key in enumerate(self.cache):
+            if key in future_indices:
+                future_indices[key] = min(future_indices[key], i)  # Update future index
+        evict_key = min(future_indices, key=future_indices.get)  # Key with the minimum future index
+        del self.cache[evict_key]  # Remove the key-value pair
+
+# Example usage:
+cache = OptimalCache(3)
+cache.put(1, 'A')
