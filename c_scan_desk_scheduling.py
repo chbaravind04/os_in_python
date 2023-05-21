93428d79e8066fae776e31e9550c6943436ece2d
chbaravind04 <118453542+chbaravind04@users.noreply.github.com>
2023-05-21 15:20:44 +0530

    Create c_scan_desk_scheduling.py

---
+++ c_scan_desk_scheduling.py
@@ -0,0 +1,35 @@
+def cscan_disk_scheduling(initial_head, disk_size, requests):
+    total_head_movement = 0
+
+    # Sort requests based on their position
+    sorted_requests = sorted(requests)
+
+    # Scan towards the right boundary
+    for i in range(initial_head, disk_size + 1):
+        if i in sorted_requests:
+            total_head_movement += abs(initial_head - i)
+            initial_head = i
+            sorted_requests.remove(i)
+
+    # Move to the beginning of the disk
+    total_head_movement += abs(initial_head - 0)
+    initial_head = 0
+
+    # Scan towards the right boundary again
+    for i in range(initial_head, disk_size + 1):
+        if i in sorted_requests:
+            total_head_movement += abs(initial_head - i)
+            initial_head = i
+            sorted_requests.remove(i)
+
+    return total_head_movement
+
+
+# Test the C-SCAN disk scheduling algorithm
+if __name__ == "__main__":
+    initial_head_position = 50
+    disk_size = 200
+    request_queue = [82, 170, 43, 140, 24, 16, 190]
+
+    total_movement = cscan_disk_scheduling(initial_head_position, disk_size, request_queue)
+    print("Total head movement:", total_movement)
