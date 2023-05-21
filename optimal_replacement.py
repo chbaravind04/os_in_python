dd8bc11bd4885f4cb1eeb2d1900d668ffa2d2d78
chbaravind04 <118453542+chbaravind04@users.noreply.github.com>
2023-05-21 15:14:27 +0530

    Update optimal_replacement.py

--- optimal_replacement.py
+++ optimal_replacement.py
@@ -27,3 +27,10 @@ class OptimalCache:
 # Example usage:
 cache = OptimalCache(3)
 cache.put(1, 'A')
+cache.put(2, 'B')
+cache.put(3, 'C')
+print(cache.get(1))  # Output: A
+print(cache.get(2))  # Output: B
+cache.put(4, 'D')
+print(cache.get(3))  # Output: -1 (not found)
+print(cache.get(4))  # Output: D
