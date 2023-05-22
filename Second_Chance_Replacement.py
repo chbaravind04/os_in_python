6e394bce936eccd6868425b702fc0a4a3c2e04fb
chbaravind04 <118453542+chbaravind04@users.noreply.github.com>
2023-05-22 10:57:19 +0530

    Update Second_Chance_Replacement.py

--- Second_Chance_Replacement.py
+++ Second_Chance_Replacement.py
@@ -1,44 +1,61 @@
-class SecondChanceCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.cache = []
-        self.pointer = 0
 
-    def get(self, key):
-        for i in range(len(self.cache)):
-            if self.cache[i][0] == key:
-                self.cache[i][1] = 1  # Set reference bit to 1
-                return self.cache[i][2]
-        return -1
+def findAndUpdate(x, arr, second_chance, frames):
+	
+	for i in range(frames):
+		
+		if arr[i] == x:
+			second_chance[i] = True
+			
+			return True
+	
+	return False
 
-    def put(self, key, value):
-        for i in range(len(self.cache)):
-            if self.cache[i][0] == key:
-                self.cache[i][2] = value  # Update value
-                self.cache[i][1] = 1  # Set reference bit to 1
-                return
-        if len(self.cache) >= self.capacity:
-            self.evict()  # Remove a key-value pair based on second-chance replacement strategy
-        self.cache.append([key, 1, value])  # Add new key-value pair with reference bit set to 1
+def replaceAndUpdate(x, arr, second_chance, frames, pointer):
+	while(True):
+	
+		if not second_chance[pointer]:
+		
+			arr[pointer] = x
+			return (pointer+1)%frames
+		
+		second_chance[pointer] = False
+		
+		pointer = (pointer + 1) % frames
 
-    def evict(self):
-        while True:
-            current = self.cache[self.pointer]
-            if current[1] == 1:
-                current[1] = 0  # Set reference bit to 0
-                self.pointer = (self.pointer + 1) % len(self.cache)  # Move pointer to next position
-            else:
-                del self.cache[self.pointer]  # Remove the key-value pair
-                self.pointer = self.pointer % len(self.cache)  # Update pointer position
-                break
+def printHitsAndFaults(reference_string, frames):
+	
+	pointer = 0
+	
+	pf = 0
+	
+	arr = [0]*frames
+	
+	for s in range(frames):
+		arr[s] = -1
+		
+	second_chance = [False]*frames
+	
+	Str = reference_string.split(' ')
+	
+	l = len(Str)
+	
+	for i in range(l):
+		x = Str[i]
+		
+		if not findAndUpdate(x,arr,second_chance,frames):
+		
+			pointer = replaceAndUpdate(x,arr,second_chance,frames,pointer)
+			
+			pf += 1
+	
+	print("Total page faults were", pf)
 
-# Example usage:
-cache = SecondChanceCache(3)
-cache.put(1, 'A')
-cache.put(2, 'B')
-cache.put(3, 'C')
-print(cache.get(1))  # Output: A
-print(cache.get(2))  # Output: B
-cache.put(4, 'D')
-print(cache.get(3))  # Output: -1 (not found)
-print(cache.get(4))  # Output: D
+
+# Test 2:
+reference_string = "2 5 10 1 2 2 6 9 1 2 10 2 6 1 2 1 6 9 5 1"
+frames = 4
+
+# Output is 11
+printHitsAndFaults(reference_string,frames)
+
+# This code is contributed by mukesh07.
